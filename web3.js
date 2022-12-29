fed482af9c2832b15a2b715e1eb240254521f0d5
venusgalstar <venusgalstar@gmail.com>
2022-12-30 00:40:04 +0900

    finish update

--- web3.js
+++ web3.js
@@ -1,6 +1,7 @@
 import {config} from './config/config.js';
 import Web3 from "web3";
 import * as database from './database.js';
+import shell from 'shelljs';
 
 //composed tx bank send pose1uekktqqhk45vl0h8s4t33pk2l7eksg2ergwua2 pose1nmh7qp0uk3zp4mj3me0x7fm5kcsqac9c2ch8gw 10pose -y
 
@@ -45,33 +46,30 @@ const monitorContract = async() =>{
                 }
     
                 var count = Object.keys(event).length;
+                var idx_n;
     
                 if( count == 0 ){
                     return;
                 }
-    
-                console.log("new event", count);
-                var idx;
-                var transactionList = [];
    
                 console.log("event", event);
 
-                for( idx = 0; idx < count; idx++ ){
+                for( idx_n = 0; idx_n < count; idx_n++ ){
                     var transaction = [];
-                    transaction["timestamp"] = event[idx].blockNumber;
-                    transaction["from"] = event[idx].returnValues[0];
-                    transaction["to"] = event[idx].returnValues[1];
-                    transaction["id"] = event[idx].returnValues[2] ;
-                    transaction["transaction_hash"] = event[idx].transactionHash;
+                    transaction["timestamp"] = event[idx_n].blockNumber;
+                    transaction["from"] = event[idx_n].returnValues[0];
+                    transaction["to"] = event[idx_n].returnValues[1];
+                    transaction["id"] = event[idx_n].returnValues[2] ;
+                    transaction["transaction_hash"] = event[idx_n].transactionHash;
 
                     if( transaction["from"] != "0x0000000000000000000000000000000000000000" ){
-                        transactionList.push(transaction);
+                        var script = "composed tx bank send " + transaction['from'] + " " + transaction['to'] + " 100pose -y";
+
+                        console.log("script", script);
+
+                        shell.exec(script);
                     }
                 }
-    
-                console.log(transactionList);
-    
-                // database.insertTransaction(transactionList);
             });
 
         }
